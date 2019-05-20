94b999762fe356522a176391fb44fc28dd21aad7
Simas <simonas@digitouch.lt>
2019-05-20 08:39:07 +0300

    add Updated configuration + eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,21 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 5
-    },
-    "rules": {
-        "no-console": 0,
-        "no-undef": 0,
-        "indent": [
-            "error",
-            "tab"
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+	env: {
+		browser: true,
+		commonjs: true,
+		node: true,
+	},
+	extends: "eslint:recommended",
+	parser: "babel-eslint",
+	parserOptions: {
+		ecmaVersion: 5,
+	},
+	rules: {
+		"no-console": 0,
+		"no-undef": 0,
+		indent: ["error", "tab"],
+		"linebreak-style": ["error", "unix"],
+		quotes: ["error", "double"],
+		semi: ["error", "always"],
+	},
+	plugins: ["html"],
 };
